50d435eb9e3c8ba7078c554f3876859755e5c514
Parth Adroja <parth2cool2008@gmail.com>
2016-11-20 14:58:02 +0530

    Initial Commit

---
+++ GooglePlacesDemo/GooglePlacesDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GooglePlacesDemo
+//
+//  Created by Parth Adroja on 20/11/16.
+//  Copyright © 2016 Parth Adroja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
